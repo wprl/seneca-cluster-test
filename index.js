9fc398051314500f6e22436272285cde53b7496e
William Riley-Land <william@kun.io>
2015-08-25 13:52:49 -0500

    Working with beanstalkd (no cluster).

--- index.js
+++ index.js
@@ -6,36 +6,34 @@ var express = require('express');
 var cpuCount = require('os').cpus().length;
 var childCount = cpuCount / 2;
 
-if (cluster.isMaster) {
-  // Fork workers.
-  for (var i = 0; i < childCount; i++) {
-    cluster.fork();
-  }
+// if (cluster.isMaster) {
+//   // Fork workers.
+//   for (var i = 0; i < 1; i++) {
+//     cluster.fork();
+//   }
 
-  cluster.on('exit', function(worker, code, signal) {
-    console.log('worker ' + worker.process.pid + ' died');
-  });
-}
-else {
+//   cluster.on('exit', function(worker, code, signal) {
+//     console.log('worker ' + worker.process.pid + ' died');
+//   });
+// }
+// else {
   var seneca1 = seneca();
 
-  seneca1.use('seneca-rabbitmq-transport')
+  seneca1.use('seneca-beanstalk-transport')
     .use('seneca-web');
 
-  seneca1.client({ type: 'tcp', port: 10301, pin: 'role:test-worker,cmd:*' })
+  seneca1.client({
+    type: 'beanstalk'
+  });
 
   // Create a web.
   seneca1.add({ role: 'test-web', cmd: 'proxy' }, function (args, done) {
     return seneca1.act({ role: 'test-worker', cmd: 'ping' }, done);
   });
 
-  seneca1.act({ role: 'web', map: [
-    { proxy: { alias: 'ping' } }
-  ]});
-
   seneca1.act({ role: 'web' }, { use: {
     prefix: '/',
-    pin: { role: 'test-web', cmd:'*' },
+    pin: { role: 'test-web', cmd: '*' },
     map:{ proxy: { GET: true, alias: 'ping' } }
   }});
 
@@ -53,12 +51,11 @@ else {
 
   var seneca2 = seneca({
     transport: {
-      type: 'tcp',
-      tcp: { port: 10301 }
+      type: 'beanstalk'
     }
   });
 
-  seneca2.use('seneca-rabbitmq-transport');
+  seneca2.use('seneca-beanstalk-transport');
 
   // Create a worker.
   seneca2.add({ role: 'test-worker', cmd: 'ping' }, function (args, done) {
@@ -66,4 +63,4 @@ else {
   });
 
   seneca2.listen();
-}
+// }
