d6ddc7e31825eadb9f07d0d7bb55d4676cc089bf
William Riley-Land <william@kun.io>
2015-08-25 10:08:54 -0500

    Stub test design.

---
+++ index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var seneca = require('seneca')();
+
+seneca.use('seneca-rabbitmq-transport');
+
+// Create the web cluster.
+
+// Create the worker cluster.
+
+// Send some messages.
